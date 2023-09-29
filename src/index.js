e3eb0e812059566988830a77e1a802e0877f55c3
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2023-09-29 16:32:42 +0300

    Created the initial setup and rendered the homepage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+
+
 const app = express();
 const PORT = 5050;
 
+//Handlebars Configuration
+app.engine('hbs', handlebars.engine({extname: 'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views') 
+
 app.get('/', (req, res) => {
-    res.send('Hello from Express Server!')
+    res.render('index');
 });
 
 app.listen(PORT, () => {
