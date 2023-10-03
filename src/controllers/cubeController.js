8249c8a8f4411a86113772a2748174c37edbb239
unknown <stefan.chervenkov.sth@gmail.com>
2023-10-03 08:22:02 +0300

    setup a database file and dynamic rendering of cubes in homecontroller

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
+
 router.get('/create', (req, res) => {
+    
     res.render('create');
 });
 
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    fs.readFile(path.resolve(__dirname, '..', 'config', 'database.json'), 'utf-8', (err, data)=> {
+        const cubesArr = JSON.parse(data);
+        res.render('index', {cubesArr});
+    });
+    
 });
 
 router.get('/about', (req, res) => {
