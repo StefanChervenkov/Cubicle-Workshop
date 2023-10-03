0c041752f9cb7aaa8b1aad46ad01c4933e96d1ea
unknown <stefan.chervenkov.sth@gmail.com>
2023-10-03 14:54:00 +0300

    implemented search function

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,7 +4,24 @@ const path = require('path');
 
 router.get('/', (req, res) => {
     fs.readFile(path.resolve(__dirname, '..', 'config', 'database.json'), 'utf-8', (err, data)=> {
-        const cubesArr = JSON.parse(data);
+        let cubesArr = JSON.parse(data);
+        
+        if (req.query.search) {
+            const searchValue = req.query.search.toLowerCase()
+            cubesArr = cubesArr.filter(cube => cube.name.toLowerCase().includes(searchValue));
+            
+        } 
+
+        if (req.query.from) {
+            const searchValue = Number(req.query.from);
+            cubesArr = cubesArr.filter(cube => Number(cube.difficultyLevel) >= searchValue);
+        };
+        if (req.query.to) {
+            const searchValue = Number(req.query.to);
+            cubesArr = cubesArr.filter(cube => Number(cube.difficultyLevel) <= searchValue);
+        };
+
+
         res.render('index', {cubesArr});
     });
     
