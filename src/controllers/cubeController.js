70f1b6a233f20de6161945530840cf47a550d512
unknown <stefan.chervenkov.sth@gmail.com>
2023-10-03 13:24:57 +0300

    setup dynamic details page routing

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,14 +1,29 @@
 const router = require('express').Router();
-
+const path = require('path');
+const fs = require('fs');
 
 router.get('/create', (req, res) => {
-    
     res.render('create');
 });
 
 router.post('/create', (req, res) => {
-    console.log(req.body);
+
     res.redirect('/create')
 });
+
+router.get('/details/*', (req, res) => {
+    const id = req.url
+        .split('/')
+        .pop();
+
+    fs.readFile(path.resolve(__dirname, '..', 'config', 'database.json'), 'utf-8', (err, data) => {
+        const cubesArr = JSON.parse(data);
+        const filteredCube = cubesArr.filter(cube => cube.id === id)[0];
+        res.render('details',  filteredCube);
+    });
+
+
+
+})
 
 module.exports = router;
