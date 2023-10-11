c505b2d37630470e39f35ac4ed905978e691f8ba
unknown <stefan.chervenkov.sth@gmail.com>
2023-10-11 15:45:41 +0300

    fixed rendering of allcubes present in the db onto the first page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
-const fs = require('fs');
+const Cube = require('../models/cube');
 const path = require('path');
 
-router.get('/', (req, res) => {
-
-    res.render('index');
+router.get('/', async (req, res) => {
+    const allCubes = await Cube.find().lean();
+    
+    res.render('index', {allCubes});
 });
     
 
 
 router.get('/about', (req, res) => {
+   
     res.render('about');
 });
 
