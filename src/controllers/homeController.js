7d7d7a2c1274853652694594c7b855c51e0676d1
unknown <stefan.chervenkov.sth@gmail.com>
2023-10-11 12:07:56 +0300

    set up and configured mongodb using mongoose

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,29 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 router.get('/', (req, res) => {
-    fs.readFile(path.resolve(__dirname, '..', 'config', 'database.json'), 'utf-8', (err, data)=> {
-        let cubesArr = JSON.parse(data);
-        
-        if (req.query.search) {
-            const searchValue = req.query.search.toLowerCase()
-            cubesArr = cubesArr.filter(cube => cube.name.toLowerCase().includes(searchValue));
-            
-        } 
 
-        if (req.query.from) {
-            const searchValue = Number(req.query.from);
-            cubesArr = cubesArr.filter(cube => Number(cube.difficultyLevel) >= searchValue);
-        };
-        if (req.query.to) {
-            const searchValue = Number(req.query.to);
-            cubesArr = cubesArr.filter(cube => Number(cube.difficultyLevel) <= searchValue);
-        };
-
-
-        res.render('index', {cubesArr});
-    });
-    
+    res.render('index');
 });
+    
+
 
 router.get('/about', (req, res) => {
     res.render('about');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // Imports
 const express = require('express');
 const handlebarsConfig = require('./config/handlebarsConfig.js');
-const expressConfig = require('./config/expressConfig.js')
+const expressConfig = require('./config/expressConfig.js');
+const dbConfig = require('./config/dbConfig.js');
 const  {PORT} = require('./constants.js');
 const routes = require('./router.js')
 
@@ -12,6 +13,7 @@ const app = express();
 //  Configuration
 expressConfig(app);
 handlebarsConfig(app);
+dbConfig();
 
 // Routing
 app.use(routes);
