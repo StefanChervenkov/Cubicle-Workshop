ce32b54be077ce0c54db5b52749377de36cbe5be
unknown <stefan.chervenkov.sth@gmail.com>
2023-10-11 15:03:53 +0300

    added accessoryController and an addAccessory view

---
+++ src/controllers/accessoryController.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+router.get('/create/accessory', (req, res) => {
+    res.render('createAccessory')
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.use(express.urlencoded({extended: false}))
 router.use(homeController);
 router.use(cubeController);
+router.use(accessoryController);
 
 router.get('*', (req, res) => {
     res.render('404');
