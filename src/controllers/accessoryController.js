6e076fa6ab09e69d62995c78bdd17e84ed350c21
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2023-10-12 09:34:50 +0300

    minor changes to attachAccessory controller

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -22,11 +22,25 @@ router.post('/create/accessory', async (req, res) => {
 router.get('/attach/accessory/:id', async (req, res) => {
     const cube = await Cube.findById(req.params.id).lean();
     const allAccessories = await Accessory.find().lean();
-    const unattachedAccessories = allAccessories.filter(accessory => !cube.accessories.includes(accessory._id.toString()) )
-    
+    const unattachedAccessories = allAccessories.filter(accessory => !cube.accessories.includes(accessory._id.toString()))
 
-    res.render('attachAccessory', {cube, unattachedAccessories} )
+
+    res.render('attachAccessory', { cube, unattachedAccessories })
+});
+
+router.post('/attach/accessory/:id', async (req, res) => {
+    const accessoryId = req.body.accessory;
+    const cubeId = req.params.id;
+    await Cube.updateOne(
+        { _id: cubeId },
+        { $push: { accessories: accessoryId } });
+
+
+
+    res.redirect('/')
 })
+
+
 
 
 
