441b92e3e78ca8e9cfcee45413bff873ac3fc919
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2023-10-17 22:27:01 +0300

    initialize User model structure

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,14 @@ router.get('/login', (req, res) => {
     res.render('login')
 });
 
+// Routes for the register
 router.get('/register', (req, res) => {
     res.render('register')
+});
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    res.redirect('/login')
 });
 
 router.get('/logout', (req, res) => {
