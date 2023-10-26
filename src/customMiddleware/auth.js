8a0353e4c14c001f7ad70d611a1ceccb992fadd3
unknown <stefan.chervenkov.sth@gmail.com>
2023-10-26 12:42:43 +0300

    fixed the authentication middleware

--- src/customMiddleware/auth.js
+++ src/customMiddleware/auth.js
@@ -3,16 +3,19 @@ const { SECRET } = require('../constants');
 
 exports.auth = async (req, res, next) => {
     const token = req.cookies.auth;
-    const decoded = await jwt.verify(token, SECRET);
-
+  
     if (token) {
+      try {
         const decoded = await jwt.verify(token, SECRET);
-
+  
         req.user = decoded;
         res.locals.user = decoded;
         res.locals.isAuthenticated = true;
-
+      } catch (err) {
+       console.log(err);
+      }
     }
-
+  
     next();
-}
+  };
+  
